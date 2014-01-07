a515599a1cb9a2b0db4bb260590c09464a150d1c
rvsharpe.ubc@gmail.com <rvsharpe.ubc@gmail.com@69186f74-b5a6-b451-f1d6-44d06303a42d>
2014-01-07 01:48:40 +0000

    Starting Field feeding data

---
+++ EcuRCode/FieldFeedingTrials.R
@@ -0,0 +1,10 @@
+# TODO: Add comment
+# 
+# Author: Ruth
+###############################################################################
+##Field feeding trials
+
+Fld_Feed <- read.csv("RuthEcuador2013/FieldFeedingTrials/FeedingData.csv", na.strings = NA)
+Fld_Trials<-read.csv("RuthEcuador2013/FieldFeedingTrials/IndTrials.csv", na.strings = NA)
+Fld_Prey <- read.csv("RuthEcuador2013/FieldFeedingTrials/PreyCapture.csv", na.strings = NA)
+Fld_Census <- read.csv("RuthEcuador2013/FieldFeedingTrials/FieldFeedingCensus.csv", na.strings = NA)
