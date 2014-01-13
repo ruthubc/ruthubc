3aaed6a037d614eccab04e0aad2778687876f2cf
rvsharpe.ubc@gmail.com <rvsharpe.ubc@gmail.com@69186f74-b5a6-b451-f1d6-44d06303a42d>
2014-01-13 00:50:11 +0000

    Including some internet resources for data analysis

--- EcuRCode/NestSizeStats.R
+++ EcuRCode/NestSizeStats.R
@@ -3,5 +3,10 @@
 # Author: Ruth
 ###############################################################################
 
+# useful pdf http://www.stat.columbia.edu/~martin/W2024/R11.pdf
+# http://www.stat.columbia.edu/~martin/W2024/W2024.html very clear
+# http://www.bodowinter.com/tutorial/bw_LME_tutorial2.pdf on page 14.
+# http://stats.stackexchange.com/questions/5344/how-to-choose-nlme-or-lme4-r-library-for-mixed-effects-models
+# book about lme4: http://lme4.r-forge.r-project.org/book/
 
-
+library(lme4)
