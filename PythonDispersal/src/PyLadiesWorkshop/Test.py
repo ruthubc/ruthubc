96cf09a1635207a36f535b19347143fc46b1c561
RuthUBC <rvsharpe.ubc@gmail.com>
2015-10-01 16:00:17 -0700

    pylades update with links

--- PythonDispersal/src/PyLadiesWorkshop/Test.py
+++ PythonDispersal/src/PyLadiesWorkshop/Test.py
@@ -10,4 +10,6 @@ import easy_install
 import virtualenv
 
 
-# instructions here
+# instructions here for changing the path https://www.youtube.com/watch?v=ddpYVA-7wq4
+# open cmd pompt as admin
+#comd: pip install virtualenv
