f26daa8084c3dcacac6cadccb797c1dc878152b8
RuthUBC <rvsharpe.ubc@gmail.com>
2016-02-09 18:09:18 -0800

    Looking at field feeding trail data

--- EcuRCode/FieldFeeding/FieldFeedingTrials.R
+++ EcuRCode/FieldFeeding/FieldFeedingTrials.R
@@ -4,7 +4,8 @@
 ###############################################################################
 ##Field feeding trials
 
-
+library(plyr)
+library(ggplot2)
 
 Fld_Feed <- read.csv("RuthEcuador2013/FieldFeedingTrials/FeedingData.csv", na.strings = NA)
 Fld_Trials<-read.csv("RuthEcuador2013/FieldFeedingTrials/IndTrials.csv", na.strings = NA)
@@ -12,10 +13,37 @@ Fld_Prey <- read.csv("RuthEcuador2013/FieldFeedingTrials/PreyCapture.csv", na.st
 Fld_Census <- read.csv("RuthEcuador2013/FieldFeedingTrials/FieldFeedingCensus.csv", na.strings = NA)
 Fld_TimeArr <- read.csv("RuthEcuador2013/FieldFeedingTrials/TimeFirstArrive.csv", na.strings = NA)
 
-Fld_Combo<- merge(Fld_Feed, Fld_Trials, by = (c("TrialID")))
+ggplot(Fld_Trials, aes(PreyLen.mm, fill = Size)) + geom_histogram()
+
+
 
 Fld_Feed$Instar <- factor(Fld_Feed$Instar, levels =c("AdFem", "Sub2", "Sub1", "Juv34", "Juv12", "Male"))
 levels(Fld_Feed$Instar)
 
-ggplot(Fld_Combo, aes(x= Instar, y = TotalTimeFeed)) + geom_boxplot() + facet_wrap(~Size, scales = "free_y")
+Fld_Combo<- merge(Fld_Feed, Fld_Trials, by = (c("TrialID")))
+
+# need proportion of total time
+
+TotTimeByTrial<- ddply(Fld_Combo, .(TrialID), summarise, # need to discount trials where no feeding obs and eve
+		TotTimeAllInd = sum(TotalTimeFeed)
+)
+
+Fld_Combo<- merge(TotTimeByTrial, Fld_Combo, by = (c("TrialID")))
+
+Fld_Combo$PropTimeFd <- Fld_Combo$TotalTimeFeed/ Fld_Combo$TotTimeAllInd
+
+Fld_Combo$PropTimeArc <- asin(sqrt(Fld_Combo$PropTimeFd))
+
+ggplot(Fld_Combo, aes(PropTimeArc)) + geom_histogram()
+
+ggplot(Fld_Combo, aes(x= Instar, y = PropTimeArc)) + geom_boxplot() + facet_wrap(~Size, scales = "free_y")
+
+ggplot(Fld_TimeArr, aes(MinsFstEat)) + geom_histogram()
+
+
+ggplot(Fld_TimeArr, aes(x= Instar, y = MinsFstEat)) + geom_boxplot()
+
+
+ggplot(DF1, aes(x = Rank, y = value, fill = variable)) + 
+		geom_bar(stat = "identity")
 
