3040dc0187e23d5d8209a15aed1b8cb30757f79d
RuthUBC <rvsharpe.ubc@gmail.com>
2016-05-31 18:13:42 -0700

    Making nicer survivorship curve

--- EcuRCode/Disperal_2012/Propagules.R
+++ EcuRCode/Disperal_2012/Propagules.R
@@ -1,14 +1,17 @@
 # Using Nathan's dispersal data
 ###############################################################################
 
-library(dplyr)
+library(plyr)
 library(survival)
+library(muhaz)
 
 props <- read.csv("PropaguleData/dispersal_propagule_data.csv")
 
 props$dateFormatted <- as.Date(props$date, format = "%d/%m/%Y")
 
-props$propID <- paste(props$nestID, props$Propagule, sep = "_")
+props$propID <- as.factor(paste(props$nestID, props$Propagule, sep = "_"))
+nlevels(props$propID)
+
 
 props <- ddply(props, "propID", transform, dateFirstRec = min(dateFormatted)) # calculating max and min
 
@@ -18,17 +21,52 @@ props$DiffDays <- difftime(props$dateFormatted, props$dateFirstRec, units = 'day
 
 props$DiffDays <- as.numeric(props$DiffDays)
 
+ggplot(props, aes(x = DiffDays)) + geom_histogram(bins = max(props$DiffDays)) + scale_x_continuous(breaks= seq(0,60,by=7)) # changes axis  tick intervals
 
 
-msurv <- with(props, Surv(DiffDays, AliveOrDeadOrEnd =="Dead", type = "right"))
+props$Dead_bin<- ifelse(props$AliveOrDeadOrEnd == "Dead", 1, 0)
+
+
+
+
+msurv <- with(props, Surv(DiffDays, Dead_bin ==1, type = "right"))
 survivalMean <- as.data.frame(mean(msurv[,1])) # don't use mean without the [,1] by itself, wrong!
 
 ## Graphing
 
 prop.survfit <- survfit(Surv(DiffDays, AliveOrDeadOrEnd =="Dead", type = "right") ~ 1, data = props)
 
-pdf("PropaguleData/SurvivalCurve.pdf")
+summary(prop.survfit)
+print(prop.survfit)#, show.rmean=T)
 
-plot(prop.survfit)
+library(survminer)
+
+
+pdf("PropaguleData/SurvivalCurve_Propagules.pdf")
+
+# this is the best graph
+## Info here http://www.sthda.com/english/wiki/survminer-r-package-survival-data-analysis-and-visualization
+
+ggsurvplot(prop.survfit,color = "#2E9FDF", break.time.by = 5, xlab = "Days", ylab = "Propagule survival probability",  censor = FALSE)
 
 dev.off()
+
+#propsHaz <- kphaz.fit(props$DiffDays, props$Dead_bin)
+#kphaz.plot(propsHaz)
+propsHaz <- muhaz(props$DiffDays, props$Dead_bin)
+plot(propsHaz)
+summary(propsHaz)
+
+library(ggplot2)
+library(survMisc)
+
+autoplot(prop.survfit, type = "CI", pVal = TRUE)
+
+library(GGally)
+
+ggsurv(prop.survfit, plot.cens = FALSE)
+
+# this is the best graph
+## Info here http://www.sthda.com/english/wiki/survminer-r-package-survival-data-analysis-and-visualization
+library(survminer)
+ggsurvplot(prop.survfit,color = "#2E9FDF", break.time.by = 5, xlab = "Days")
