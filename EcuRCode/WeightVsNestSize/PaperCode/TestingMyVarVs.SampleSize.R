9484995817c0ce4820a24069912a26bea8e33cd1
RuthUBC <rvsharpe.ubc@gmail.com>
2016-07-27 16:14:24 -0700

    bootstrap variance

--- EcuRCode/WeightVsNestSize/PaperCode/TestingMyVarVs.SampleSize.R
+++ EcuRCode/WeightVsNestSize/PaperCode/TestingMyVarVs.SampleSize.R
@@ -206,3 +206,76 @@ filtered <- filter(spidersBootAve, mean > 14.990 & mean < 15.001)
 ggplot(filtered, aes(x = exp(sd_data))) + geom_histogram()
 
 ggplot(filtered, aes(x = asin(sqrt(relativeVar)))) + geom_histogram()
+
+
+########## Getting cumulative frequency distribution
+
+nestSD <- spidersBootAve$sd_data
+
+min_sd <- min(spidersBootAve$sd_data)
+max_sd <- max(spidersBootAve$sd_data)
+
+breaks = seq(min_sd, max_sd, by = 0.5)
+
+duration.cut = cut(nestSD, breaks, right=FALSE) 
+duration.freq = table(duration.cut)
+duration.cumfreq = cumsum(duration.freq)
+
+cbind(duration.cumfreq)
+
+
+#########
+library(SciencesPo)
+a_test<- detail(spidersBootAve)
+
+a_test <- freq(spidersBootAve$sd_data)
+
+a_freqBins <- as.character(a_test$class)
+
+library(stringr)
+
+str_replace("don[", "[[]", "t")
+
+a_freqBins<- gsub("]", "", a_freqBins)
+a_freqBins<- gsub("\\(", "", a_freqBins)
+a_freqBins <- gsub("\\[", "", a_freqBins)
+
+#a_test<- 
+a_freqBins <- str_split(a_freqBins, ",")
+
+a_table <- read.table(text = a_freqBins, sep = ",",  colClasses = "character", col.names = c("name", "name2"))
+
+an_table <- cbind(a_table, a_test)
+
+
+
+str_replace(fruits, "[aeiou]", "-")
+str_replace_all(fruits, "[aeiou]", "-")
+str_replace(fruits, "([aeiou])", "")
+str_replace(fruits, "([aeiou])", "\\1\\1")
+str_replace(fruits, "[aeiou]", c("1", "2", "3"))
+str_replace(fruits, c("a", "e", "i"), "-")
+fruits <- c("one apple", "two pears", "three bananas")
+str_replace(fruits, "[aeiou]", "-")
+str_replace_all(fruits, "[aeiou]", "-")
+str_replace_all(fruits, "([aeiou])", "")
+str_replace_all(fruits, "([aeiou])", "\\1\\1")
+str_replace_all(fruits, "[aeiou]", c("1", "2", "3"))
+str_replace_all(fruits, c("a", "e", "i"), "-")
+
+
+fruits <- c(
+		"apples and oranges and pears and bananas",
+		"pineapples and mangos and guavas"
+)
+a_test<- str_split(fruits, " and ")
+# Specify n to restrict the number of possible matches
+18 str_sub
+str_split(fruits, " and ", n = 3)
+str_split(fruits, " and ", n = 2)
+# If n greater than number of pieces, no padding occurs
+str_split(fruits, " and ", n = 5)
+# Use fixed to return a character matrix
+str_split_fixed(fruits, " and ", 3)
+str_split_fixed(fruits, " and ", 4)
+
