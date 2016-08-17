5de1c5cdd3e538ee62ec038160933cbe6ce334fc
ruthubc <Amber22Ruby>
2016-08-16 19:08:03 -0700

    creating results docx file

---
+++ EcuRCode/WeightVsNestSize/PaperCode/CurrentPaperCode_9Aug/Function_AnovaResultsOutputDocx.R
@@ -0,0 +1,38 @@
+# TODO: Add comment
+# 
+# Author: user
+###############################################################################
+
+
+outputResultsWord <- function(FullModel, RedModel) {
+	
+	outputModel <- anova(FullModel, RedModel)
+	
+	lowDF <- outputModel[[1L]][1]
+	
+	highDF <- outputModel[[1L]][2]
+	
+	ChiSqr <- round(outputModel[[6L]][2], digits = 2)
+	
+	pValue <- round(outputModel[[8L]][2], digits = 3)
+	
+	if (pValue <= 0.001) {
+		pValue <- "< 0.001"
+		stars <- "*** "
+	}else if (pValue <= 0.01) {
+		stars <- "** "
+		
+	}else if (pValue <= 0.05) {
+		stars <- "** "
+	
+	}else{
+		stars <-""
+	}
+	
+	textOutput <- paste("chisqr", lowDF, highDF, "=" , ChiSqr, ", p =",
+			pValue, stars, sep = "")
+	outputList <- list(lowDF, highDF, ChiSqr, pValue, stars, textOutput)
+	
+	
+}
+
--- EcuRCode/WeightVsNestSize/PaperCode/CurrentPaperCode_9Aug/Stats_PropVsOrginalNest.R
+++ EcuRCode/WeightVsNestSize/PaperCode/CurrentPaperCode_9Aug/Stats_PropVsOrginalNest.R
@@ -4,16 +4,59 @@
 ###############################################################################
 
 
-nestsSigOnly<- subset(spiders, type == "multiple")
-length(nestsSigOnly)
+nestsSigOnly<- subset(spiders, type != "multiple" & Instar == "Adult")
+
+OrgNests <- subset(spidersMul, Instar == "Adult")
+OrgNests <- subset(OrgNests, NestID == "44.4EX03" | NestID == "28.8EX15")
+OrgNests$OrigNest <- OrgNests$NestID
 
 
-ggplot(Spis44, aes(x=type, y=logcond)) + geom_boxplot() + 
-		ggtitle("Hunger 44.4 nests only ") + ylab("log Cond") + mytheme +theme(axis.title.x = element_blank()) +
+
+nestsSigOnly$OrigNest <- ifelse(grepl("^4",as.character(nestsSigOnly$NestID)) == TRUE, "44.4EX03", "28.8EX15")
+
+
+nestsToTestSig <- rbind(nestsSigOnly, OrgNests)
+
+nestsSigOnlyCond<- subset(nestsSigOnly, nestsSigOnly$FemalesHaveEggsOrJuvs == "y")
+
+nestsToTestSigCond <- rbind(nestsSigOnlyCond, OrgNests)
+
+## Testing leg length
+FullModel <- lmer(logLeg ~  type  +  (1|NestID) + (1|OrigNest) , data = nestsToTestSig, REML = FALSE)
+
+anova(FullModel)
+
+RedModel <- lmer(logLeg ~  (1|NestID) + (1|OrigNest) , data = nestsToTestSig, REML = FALSE)
+
+anova(FullModel, RedModel)
+
+
+## Testing condition
+FullModel <- lmer(condResiduals ~  type  +  (1|NestID) + (1|OrigNest) , data = nestsToTestSig, REML = FALSE)
+
+anova(FullModel)
+
+RedModel <- lmer(condResiduals ~  (1|NestID) + (1|OrigNest) , data = nestsToTestSig, REML = FALSE)
+
+anova(FullModel, RedModel)
+
+
+
+# Only remove nests with ads or juvs for condition calculations
+
+mytheme <-theme_bw(base_size=15)  + theme(plot.title = element_text(vjust=2), panel.margin= unit(0.75, "lines"), axis.title.y = element_text(vjust=0),
+		plot.margin=unit(c(1,1,1.5,1.2),"cm"), panel.border = element_rect(fill = NA, colour = "grey", linetype=1, size = 1), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+
+
+p1 <- ggplot(nestsToTestSigCond, aes(x=type, y=condResiduals)) + geom_boxplot() + 
+		 ylab("Individual Condition") + mytheme+ theme(axis.title.x = element_blank()) +
 		scale_x_discrete(breaks=c("multiple", "single"), labels=c("source: multiple", "propagules: single"))
 
-ggplot(Spis44, aes(x=type, y=logLeg)) + geom_boxplot() +
-		ggtitle("Leg Length 44.4 nests only ") + ylab("Log Leg Length") +
+p2 <- ggplot(nestsToTestSig, aes(x=type, y=logLeg)) + geom_boxplot() +
+		ggtitle("Leg Length") + ylab("Log Leg Length") +
 		mytheme +  theme(axis.title.x = element_blank()) +
 		scale_x_discrete(breaks=c("multiple", "single"), labels=c("source: multiple", "propagules: single"))
+
+grid.arrange(p1, p2, ncol = 2)
 
