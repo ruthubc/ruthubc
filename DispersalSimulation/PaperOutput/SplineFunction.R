3885e841571028070059f762d9462b79cbe15960
ruthubc <Amber22Ruby>
2017-03-01 19:09:33 -0800

    spline

--- DispersalSimulation/PaperOutput/SplineFunction.R
+++ DispersalSimulation/PaperOutput/SplineFunction.R
@@ -20,14 +20,15 @@ fnSpline<-function(my.lambda, xx, yy) {
 		my.spar<-s0 + 0.0601*log(my.lambda) #getting the correct spar from my lambda  
 		
 		
-		my.spline<-smooth.spline(xx, y=yy, spar=my.spar, keep.data = TRUE)
+		myspline<-smooth.spline(xx, y=yy, spar=my.spar, keep.data = TRUE)
 	
 		print(my.spar)
 	}
 		
-	#spline.list <- list(my.spline$x, my.spline$y)
+	spline.df <- data.frame(myspline$x, myspline$y)
 		
-	return (my.spline)
+	return (spline.df)
+	#return (my.spline)
 }
 
 
@@ -35,9 +36,31 @@ fnSpline<-function(my.lambda, xx, yy) {
 splineOutput <- fnSpline(0.05, subDispFile$Comp_meas, log10(subDispFile$metPopAgeMax))
 
 
-ggplot(subDispFile, aes(x = Comp_meas, y = log10(metPopAgeMax), colour = as.factor(ad_dsp_fd))) + 
+ggplot(splineOutput, aes(x = myspline.x, y =  myspline.y))  + 
 		geom_point(size = pointSize, position = position_jitter(w = 0, h = 0.05)) + 
-		stat_smooth(method = "spline") + 
+		geom_line() + 
+		mytheme + ylab("Log max metapopulation age") + 
+		scale_colour_discrete("Size needed to disperse") + xlab("Competition measure") +
+		scale_y_continuous(breaks=seq(0, 3.0, by=0.5), limits = c(0, 3.0))
+
+
+
+ggplot() + geom_point(data = subDispFile, aes(x = Comp_meas, y = log10(metPopAgeMax), colour = as.factor(ad_dsp_fd))) + 
+		geom_line(data = splineOutput, aes(x = myspline.x, y =  myspline.y,  colour = as.factor(ad_dsp_fd))) + 
+		mytheme + ylab("Log max metapopulation age") + 
+		scale_colour_discrete("Size needed to disperse") + xlab("Competition measure") +
+		scale_y_continuous(breaks=seq(0, 3.0, by=0.5), limits = c(0, 3.0))
+
+
+splineOutput <- ddply(subDispFile, "ad_dsp_fd", function(x) {
+    		spline <- fnSpline(0.0005, x$Comp_meas, log10(x$metPopAgeMax))
+
+		})
+
+
+ggplot(splineOutput, aes(x = myspline.x, y =  myspline.y))  + 
+		geom_point(size = pointSize, position = position_jitter(w = 0, h = 0.05)) + 
+		geom_line() + 
 		mytheme + ylab("Log max metapopulation age") + 
 		scale_colour_discrete("Size needed to disperse") + xlab("Competition measure") +
 		scale_y_continuous(breaks=seq(0, 3.0, by=0.5), limits = c(0, 3.0))
