c70e31f986d258a396dff6f479536c1941b7eff2
Ruth <Ruth@137.82.80.219>
2017-03-23 18:55:50 -0700

    checkoutputfiles

--- PythonDispersal/src/deleteFiles.py
+++ PythonDispersal/src/deleteFiles.py
@@ -4,23 +4,20 @@ Created on Mar 22, 2017
 @author: Ruth
 '''
 
-#import pandas as pd
 import os
 
-#CSV = pd.read_csv('filesToDelete.csv')
 
 import csv
-with open('filesToDelete.csv', 'rb') as csvfile:
+with open('CSV_files_to_delete.csv', 'rb') as csvfile:
     myFile = csv.reader(csvfile, delimiter=',')
     for row in myFile:
         runName = ', '.join(row)
         fileName = runName + ".csv"
-        print fileName
         try:
             os.remove(fileName)
-            print "file deleted"
+            print fileName, "file deleted"
         except OSError:
-            print "file not found"
+            print fileName, "file not found"
         
 
 
