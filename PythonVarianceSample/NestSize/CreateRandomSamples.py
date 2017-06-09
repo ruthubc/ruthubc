a187fd75551cf57cd5e3a8ea0dc156393ad4306c
Ruth <Ruth@137.82.81.112>
2017-06-09 12:50:20 -0700

    creat rn samples

---
+++ PythonVarianceSample/NestSize/CreateRandomSamples.py
@@ -0,0 +1,36 @@
+'''
+Created on Aug 14, 2016
+
+@author: Ruth check
+'''
+
+
+import pandas as pd
+from random import uniform
+from scipy.stats import cumfreq
+import numpy as np
+import os
+
+#ln leg length max and min adults  0.88 and 1.33
+
+# sample size 1-25
+
+minValuePos = 0.88
+
+maxValuePos = 1.33
+
+sampleSizes = range(1, 26)
+
+i = 0
+
+columns = ['SampleID','SampleSize', 'Value']
+df = pd.DataFrame(columns = columns)
+
+def createRanSmpFun(smpSize, minVal, maxVal, df):    
+    i = i + 1
+    sampID = [i] * smpSize
+    sampSize = [smpSize] * smpSize
+    ranList =[uniform(minVal, maxVal) for p in range(0,(smpSize))]
+    
+    
+
